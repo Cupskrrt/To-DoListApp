f7f3d93ae394e8a61cd73839c1470d4f11f67476
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-17 21:00:24 +0700

    Initial Commit

---
+++ Shared/To_DoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoListAppApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct To_DoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
